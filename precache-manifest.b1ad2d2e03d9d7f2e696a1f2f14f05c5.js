ec8834a1a7c3a0713479a042db0f0eb509c3f058
hisoka1638 <1399952343@qq.com>
2022-04-17 22:06:23 +0800

    deploy

---
+++ precache-manifest.b1ad2d2e03d9d7f2e696a1f2f14f05c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a07a7577c8624336c53",
+    "url": "/mknight-website/css/app.fb3f71a3.css"
+  },
+  {
+    "revision": "cec2f84e1513d43174126058b973912b",
+    "url": "/mknight-website/index.html"
+  },
+  {
+    "revision": "5a07a7577c8624336c53",
+    "url": "/mknight-website/js/app.afcfe3ec.js"
+  },
+  {
+    "revision": "d5f9ddd3bcf73a631acf",
+    "url": "/mknight-website/js/chunk-vendors.383d98cd.js"
+  },
+  {
+    "revision": "b3a3051595f65d89667d265904df88bc",
+    "url": "/mknight-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mknight-website/robots.txt"
+  }
+]);
